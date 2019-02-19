e596967587a69fa0fe52a586bcaca2072cedb26a
Christian Zufferey <christian@zufferey.com>
2019-02-19 11:33:01 +0100

    Petits tests pour juste voir comment configurer Traefik via un fichier traefik.toml

---
+++ start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#Petit script pour démarrer tout le binz
+#zf190219.0920
+
+#pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
+#https://github.com/zuzu59/docker_demo
+
+#source: https://docs.traefik.io/
+
+#docker-compose up
+docker-compose up -d
+docker-compose logs -f
+
+
+echo -e "
+
+pour voir les logs en continu:
+docker-compose logs -f
+
+pour voir qu'est-ce qui tourne:
+docker-compose ps
+
+pour 'entrer' dans un 'service':
+docker-compose exec nom_service /bin/bash
+docker-compose exec wordpress /bin/bash
+docker-compose exec db /bin/bash
+
+pour arrêter:
+docker-compose stop
+
+pour redémarrer après un 'stop':
+docker-compose start
+
+pour enlever les container mais pas les datas:
+docker-compose down
+
+pour enlever les container ET aussi les datas:
+docker-compose down -v --remove-orphans
+
+
+"
+
+
