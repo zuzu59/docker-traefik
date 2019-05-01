b86ed4a73bddbf4a1bf6c25efc0f7bee6d8a9cca
Christian Zufferey <christian@zufferey.com>
2019-05-01 16:35:29 +0200

    Voilà, mon isolation de ports de containers fonctionne très bien. C'est la dernière fois ici que l'on travaille avec les tototutu.xx !

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Petit script pour démarrer tout le binz
-#zf190430.1659
+#zf190501.1601
 
 #pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
 #https://github.com/zuzu59/docker_demo
 
 #source: https://docs.traefik.io/
 
-docker network create traefik
+docker network create frontend
 #docker-compose up
 docker-compose up -d
 docker-compose logs -f
