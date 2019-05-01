6de80672b248be1fbe54e4ee3bcb38898ae85a3d
Christian Zufferey <christian@zufferey.com>
2019-05-01 15:20:15 +0200

    Voilà, je suis retombé sur mes pieds, le https fonctionne maintenant

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Petit script pour démarrer tout le binz
-#zf190426.1759
+#zf190430.1659
 
 #pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
 #https://github.com/zuzu59/docker_demo
 
 #source: https://docs.traefik.io/
 
+docker network create traefik
 #docker-compose up
-docker-compose down
 docker-compose up -d
 docker-compose logs -f
 
@@ -22,9 +22,7 @@ pour voir qu'est-ce qui tourne:
 docker-compose ps
 
 pour 'entrer' dans un 'service':
-docker-compose exec nom_service /bin/bash
-docker-compose exec wordpress /bin/bash
-docker-compose exec db /bin/bash
+docker-compose exec proxy /bin/sh
 
 pour arrêter:
 docker-compose stop
@@ -40,5 +38,3 @@ docker-compose down -v --remove-orphans
 
 
 "
-
-
