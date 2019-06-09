a1aaf2ee41e06d11ae319ac448b8ce1019fdf3b5
Christian Zufferey <christian@zufferey.com>
2019-06-09 16:20:40 +0200

    Amélioré le script de clean des logs ;-)

--- clean_logs.sh
+++ clean_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour nettoyer les logs de traefik
-#zf190609.1559
+#zf190609.1620
 
 #source: 
 
@@ -11,13 +11,18 @@ Usage:
 sudo ./clean_logs.sh
 
 "
-read -p "Appuyer une touche pour démarrer !"
+#read -p "Appuyer une touche pour démarrer !"
 
 echo ---------- start
 
 
+# ne garde que les 1 000 000 dernières lignes des logs !
 
 tail -n 1000000 log/traefik.log > log/t1.log
 mv log/t1.log log/traefik.log
 ls -alrt log
+
+docker-compose restart
+
+wc -l ./log/traefik.log
 
